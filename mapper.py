dc7f7408e40bca7760d34293d08f3b6668d70fd8
Yunshi <yl2612@nyu.edu>
2015-02-19 20:53:07 -0500

    change format

--- mapper.py
+++ mapper.py
@@ -4,6 +4,8 @@ import sys
 
 for line in sys.stdin:          
     split_word = line.strip().split()
-    print "%s\t%s" % (split_word[0], split_word[1:-1])
+    li = split_word[0:-1]
+    print '%s' % '\t'.join(map(str, li))
     for i in range(len(split_word)-2):
-        print "%s\t%s" % (split_word[i+1], [split_word[0], float(split_word[-1])/(len(split_word)-2)])        
+        mylist = [split_word[i+1], split_word[0], float(split_word[-1])/(len(split_word)-2)]      
+        print '%s' % '\t'.join(map(str, mylist))
--- reducer.py
+++ reducer.py
@@ -4,27 +4,23 @@ import re
 page_count_dict = {}
 page_final_dict = {}
 for line in sys.stdin:
-    line = line.strip()
-    (word, count) = line.split("\t")
+    split_word = line.strip().split()
     #page_count_dict = {"A":0, "B":0, "C":0, "D":0, "E":0, "F":0}
-    count_list = count.strip("[]").split(",")
-    rank = count_list[-1].strip()
-
+    item = split_word[0]
+    count_list = split_word[1:]
+    rank = split_word[-1]
     
-    if word in page_count_dict:
+    if item in page_count_dict:
         try:
-            page_count_dict[word] += float(rank)
+            page_count_dict[item] += float(rank)
         except:
-            page_final_dict[word] = count_list
+            page_final_dict[item] = count_list
     else:
         try:
-            page_count_dict[word] = float(rank)
+            page_count_dict[item] = float(rank)
         except:
-            page_final_dict[word] = count_list
-
-#page_final_dict = dict(((key, [page_link_dict[key], page_count_dict[key]]) for key in page_link_dict))
+            page_final_dict[item] = count_list
 
 for key in page_final_dict:
     page_final_dict[key].append(page_count_dict[key])
-    print "%s\t%s" % (key,page_final_dict[key])
-
+    print key + "\t" + "%s" % "\t".join (map(str,page_final_dict[key]))
